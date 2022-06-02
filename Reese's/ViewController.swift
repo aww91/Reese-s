4e8c7bccc1d64dd105e86b61abe2b890f0b33ba7
Anthony Whitfield <aww91@yahoo.com>
2022-06-02 09:43:24 -0400

    New version

--- Reese's/ViewController.swift
+++ Reese's/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if messageLabel.text == "You Got Peanut Butter in my Chocolate!" {
+        messageLabel.text = "You Got Chocolate in my Peanut Butter!"
+        messageLabel.textColor = UIColor.orange
+            messageLabel.textAlignment = NSTextAlignment.right
+        }  else {
+            messageLabel.text = "You Got Peanut Butter in my Chocolate!"
+            messageLabel.textColor = UIColor.brown
+            messageLabel.textAlignment = .left
+    }
+    
 }
 
+}
